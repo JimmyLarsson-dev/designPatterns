f762682eb860578370bd5e0a6b8d87fe7d343fcf
Jimmy Larsson <jimmy.larsson@europe.com>
2024-07-10 10:14:59 +0200

    Added Main classes for all packages for easier testing. Removed original Main class. Added Product class for builder pattern.

--- src/main/java/Factory/Main.java
+++ src/main/java/Factory/Main.java
@@ -1,15 +1,10 @@
-import Factory.User;
-import Factory.UserFactory;
-import singleton.Config;
+package Factory;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        Config config = Config.getUser();
-        System.out.println(config.someInfo);
         User user = UserFactory.getUser(true, "jimmy", "jimmy@mail.com");
         System.out.println(user.getEmail());
-
     }
+
 }
---
+++ src/main/java/builder/Product.java
@@ -0,0 +1,47 @@
+package builder;
+
+public class Product {
+    private String name;
+    private String description;
+    private int price;
+
+    public Product(ProductBuilder builder) {
+        this.name = builder.name;
+        this.description = builder.description;
+        this.price = builder.price;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public int getPrice() {
+        return this.price;
+    }
+
+    public static class ProductBuilder {
+        private String name;
+        private String description;
+        private int price;
+
+        public ProductBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public ProductBuilder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public ProductBuilder setPrice(int price) {
+            this.price = price;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
+    }
+}
